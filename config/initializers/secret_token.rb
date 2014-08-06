0e8ce3823cc53dec4d95c4923dfebd32d7ffc805
maiouchy <eng.mai.seif@gmail.com>
2014-08-06 18:46:26 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trygem::Application.config.secret_token = 'b5cc1d0728e0ed7dc0349dc25444638fa2ff3c4e267f8c5104379ebaf36fa211619172a8f9c7a558df6f0b21755af796afea423bbcca8be169de3150d7dbc2f5'
